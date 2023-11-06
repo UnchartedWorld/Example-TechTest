b8bb33308fdd00cd84116598f464753b8bfe5120
UnchartedWorld <nathangvjcollins@gmail.com>
2023-11-06 19:17:50 +0000

    feat(models): Creates Inital Models for Database

---
+++ Models/TableMaritalStatus.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MotivWebApp.Models
+{
+    public class TableMaritalStatus
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int MaritalStatusID { get; set; }
+        public string MaritalStatusName { get; set; }
+    }
+}
