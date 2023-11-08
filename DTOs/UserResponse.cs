7a3d05da98e1831883bfa846a566d8c540bca6f6
UnchartedWorld <nathangvjcollins@gmail.com>
2023-11-08 19:21:00 +0000

    feat(dtos): Adds UserResponse DTO
    This should make sure I only get the EXACT elements I want, ignoring the messy
    way I handled this.

---
+++ DTOs/UserResponse.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.Json.Serialization;
+
+namespace MotivWebApp.DTOs
+{
+    public class UserResponse
+    {
+        [JsonPropertyName("title")]
+        public string NameTitle { get; set; }
+        public string Name { get; set; }
+        [JsonPropertyName("medium")]
+        public string PictureURL { get; set; }
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+        [JsonPropertyName("date")]
+        public string DateOfBirth { get; set; }
+    }
+}
